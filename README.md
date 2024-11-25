eee9fbaca8175f7b7326012a553bd0d865bbae10
Thanh Duong <thanh.duong.vs@gmail.com>
2024-11-25 15:11:41 +0700

    init main

---
+++ README.md
@@ -0,0 +1,9 @@
+# KIWI_FPGA_TINY_1P5_EVK
+
+![kiwi-tiny-1p5-evk](kiwi-tiny-1p5-evk.jpg "kiwi-tiny-1p5-evk").
+
+## Example
+
+### test_led
+
+An example to drive all gpio.
---
+++ test_leds/src/led.v
@@ -0,0 +1,34 @@
+module led#(
+    parameter led_number = 34
+)
+(
+    input sys_clk,
+    output [ led_number-1 :0] leds
+);
+
+reg count_1s_flag;
+reg [23:0] count_1s = 'd0;
+
+always @(posedge sys_clk ) begin
+    if( count_1s < 12000000 ) begin
+        count_1s <= count_1s + 'd1;
+        count_1s_flag <= 'd0;
+    end
+    else begin
+        count_1s <= 'd0;
+        count_1s_flag <= 'd1;
+    end
+end
+
+
+reg [led_number-1:0] leds_value = 'd0;
+
+always @(posedge sys_clk ) begin
+    if( count_1s_flag ) begin
+    leds_value[ led_number-1 :0] <= ~ leds_value[ led_number-1 :0];
+    end
+end
+
+assign leds = ~leds_value;
+
+endmodule
